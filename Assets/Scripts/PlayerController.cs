51702b74187acbecacfbcf0e4fcb6700fe4d5faf
JamieCCH <jamie.cch@gmail.com>
2019-02-01 18:17:35 -0500

    done player movement and jump (anim)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,38 +3,152 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
-//isGroiunded need to be set
-
 public class PlayerController : MonoBehaviour {
 
-    private float speed = 10.0f;
+    private float moveSpeed = 2.0f;
     private float rotationSpeed = 100.0f;
     private float jumpSpeed = 8.0f;
+    private float jumpForce = 4;
     private float gravity = 20.0f;
 
-    private bool isGrounded = true;
+    private float interpolation = 10;
+    private float m_currentV = 0;
+    private float m_currentH = 0;
+
+    private float m_jumpTimeStamp = 0;
+    private float m_minJumpInterval = 0.25f;
+
+    private bool isGrounded;
+    private bool wasGrounded;
+    private Animator playerAnim;
+
+    private Rigidbody m_rb;
+    private List<Collider> m_collisions = new List<Collider>();
 
     // Use this for initialization
-    void Start () {
-
+    void Start()
+    {
+        playerAnim = gameObject.GetComponent<Animator>();
+        m_rb = gameObject.GetComponent<Rigidbody>();
     }
-	
-	// Update is called once per frame
-    void Update () {
 
-        if (isGrounded)
+    private void OnCollisionEnter(Collision collision)
+    {
+        //isGrounded |= collision.gameObject.name == "Terrian";
+        //Debug.Log("stay :" + isGrounded);
+
+        ContactPoint[] contactPoints = collision.contacts;
+        for (int i = 0; i < contactPoints.Length; i++)
         {
-            var x = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
-            var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-            transform.Rotate(0, x, 0);
-            transform.Translate(0, 0, z);
-
-            if (Input.GetButton("Jump"))
+            if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
             {
-                var y = gravity * Time.deltaTime;
-                transform.Translate(0, y, z);
+                if (!m_collisions.Contains(collision.collider))
+                {
+                    m_collisions.Add(collision.collider);
+                }
+                isGrounded = true;
             }
         }
     }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        //isGrounded |= collision.gameObject.name == "Terrian";
+        //Debug.Log("stay :" + isGrounded);
+
+        ContactPoint[] contactPoints = collision.contacts;
+        bool validSurfaceNormal = false;
+        for (int i = 0; i < contactPoints.Length; i++)
+        {
+            if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
+            {
+                validSurfaceNormal = true; break;
+            }
+        }
+
+        if (validSurfaceNormal)
+        {
+            isGrounded = true;
+            if (!m_collisions.Contains(collision.collider))
+            {
+                m_collisions.Add(collision.collider);
+            }
+        }
+        else
+        {
+            if (m_collisions.Contains(collision.collider))
+            {
+                m_collisions.Remove(collision.collider);
+            }
+            if (m_collisions.Count == 0) { isGrounded = false; }
+        }
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        //isGrounded &= collision.gameObject.name != "Terrian";
+        //Debug.Log("exit :" + isGrounded);
+        if (m_collisions.Contains(collision.collider))
+        {
+            m_collisions.Remove(collision.collider);
+        }
+        isGrounded &= m_collisions.Count != 0;
+    }
+
+    private void JumpingAndLanding()
+    {
+        bool jumpCooldownOver = (Time.time - m_jumpTimeStamp) >= m_minJumpInterval;
+
+        if (jumpCooldownOver && isGrounded && Input.GetButton("Jump"))
+        {
+            m_jumpTimeStamp = Time.time;
+            m_rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        if (!wasGrounded && isGrounded)
+        {
+            playerAnim.SetTrigger("Land");
+        }
+
+        if (!isGrounded && wasGrounded)
+        {
+            playerAnim.SetTrigger("Jump");
+        }
+    }
+
+    private void MovementUpdate()
+    {
+        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+
+        bool walk = Input.GetKey(KeyCode.LeftShift);
+
+        if (walk)
+        {
+            z *= moveSpeed;
+        }
+
+        m_currentV = Mathf.Lerp(m_currentV, z, Time.deltaTime * interpolation);
+        m_currentH = Mathf.Lerp(m_currentH, x, Time.deltaTime * interpolation);
+
+        transform.position += transform.forward * m_currentV * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, m_currentH * rotationSpeed * Time.deltaTime, 0);
+
+        playerAnim.SetFloat("MoveSpeed", m_currentV);
+
+        JumpingAndLanding();
+
+    }
+
+    void Update()
+    {
+        playerAnim.SetBool("Grounded", isGrounded); 
+
+        MovementUpdate();
+
+        wasGrounded = isGrounded;
+
+    }
+
 }
