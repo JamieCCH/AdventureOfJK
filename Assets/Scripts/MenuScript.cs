b03794f5c51496424c4157897774058685f7f2e0
JamieCCH <jamie.cch@gmail.com>
2019-04-01 22:35:16 -0400

    Add menu and pause screen

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MenuScript : MonoBehaviour
+{
+
+    public Button startBt;
+    public Button introBt;
+    public Button creditBt;
+    public Button exitBt;
+ 
+
+    void Start()
+    {
+        AudioSource buttonSound = GetComponent<AudioSource>();
+
+        startBt.onClick.AddListener(buttonSound.Play);
+        introBt.onClick.AddListener(buttonSound.Play);
+        creditBt.onClick.AddListener(buttonSound.Play);
+        exitBt.onClick.AddListener(buttonSound.Play);
+       
+        startBt.onClick.AddListener(() => SceneManager.LoadScene("GameLv1"));
+        introBt.onClick.AddListener(() => SceneManager.LoadScene("Intro"));
+        creditBt.onClick.AddListener(() => SceneManager.LoadScene("Credit"));
+        exitBt.onClick.AddListener(exit);
+    }
+
+
+    void exit()
+    {
+        Debug.Log("Quit Game!!");
+        Application.Quit();
+    }
+
+}
--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
 
 public class UIScript : MonoBehaviour {
 
@@ -10,10 +14,34 @@ public class UIScript : MonoBehaviour {
     private int pickupedShows = 0;
     private Slider healthSlider;
 
+    public Button exitBt;
+    public Button cancelBt;
+    public GameObject PauseScreen;
+
     void Start () {
         player = GameObject.FindWithTag("Player").GetComponent<PlayerData>();
         BallIcons = GameObject.FindGameObjectsWithTag("BallIcon");
         healthSlider = GameObject.Find("HealthSlider").GetComponent<Slider>();
+
+        for (int i = 0; i < BallIcons.Length; i++)
+        {
+            BallIcons[i] = GameObject.Find("BallIcon" + i);
+        }
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            PauseScreen.SetActive(true);
+            //Time.timeScale = 0; //PauseGame
+        }
+
+        if (PauseScreen.activeSelf == true)
+        {
+            Choice(Exit, ClosePanel);
+        }
     }
 
     private void LateUpdate()
@@ -29,5 +57,37 @@ public class UIScript : MonoBehaviour {
             ballImg.color = new Color(0.035f, 1f, 1f, 1f);
             pickupedShows++;
         }
+    }
+
+    private void ClosePanel()
+    {
+        if (PauseScreen.activeSelf == true)
+        {
+            PauseScreen.SetActive(false);
+
+            //Time.timeScale = 1;  //ContinueGame
+        }
+    }
+
+    private void Exit()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Choice(UnityAction exitEvent, UnityAction cancelEvent)
+    {
+        PauseScreen.SetActive(true);
+
+        exitBt.onClick.RemoveAllListeners();
+        exitBt.onClick.AddListener(ClosePanel);
+        exitBt.onClick.AddListener(exitEvent);
+
+        cancelBt.onClick.RemoveAllListeners();
+        cancelBt.onClick.AddListener(ClosePanel);
+
+        AudioSource buttonSound = GetComponent<AudioSource>();
+        exitBt.onClick.AddListener(buttonSound.Play);
+        cancelBt.onClick.AddListener(buttonSound.Play);
+
     }
 }
